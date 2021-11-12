69c8706876eb3baca18d99d8ad5d557a7ada23ab
vino04003 <89254939+vino04003@users.noreply.github.com>
2021-11-12 15:07:29 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+public class Program {
+
+	static bool isPalindrome(int num) {
+		int tempValue = num;
+		int reverse = 0;
+		while (tempValue > 0) {
+			reverse = reverse * 10 + tempValue % 10;
+			tempValue = tempValue / 10;
+		}
+
+		return reverse == num;
+	}
+
+	public static void Main(string[] args) {
+		int num;
+		Console.WriteLine("Skriv in ett nummer :");
+		num = int.Parse(Console.ReadLine());
+
+		if (isPalindrome(num)) {
+			Console.WriteLine("Ditt nummer är ett palindrom");
+		} else {
+			Console.WriteLine("Ditt nummer är inte ett palindrom");
+		}
+	}
+}
